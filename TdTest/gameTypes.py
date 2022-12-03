3be22cf34147537970661803b89bbe68b7bc3cb5
danbowenairstrip <danbowenairstrip@gmail.com>
2022-12-03 11:32:49 -0700

    Add wave feature. Rename types module to gameTypes.

--- TdTest/gameTypes.py
+++ TdTest/gameTypes.py
@@ -15,23 +15,33 @@ class Nme:
     maxHealth=0
     health=0
     dmgMods=[1,1,1,1]
+    spawnWave=0
+    spawnDelay=0
     lastMove=datetime.datetime.now()
     nextWaypointIndex=1
+    isSpawned=False
     isDead=False
 
-    def __init__(self, maxHealth, speed, dmgMods):
+    def __init__(self, maxHealth, speed, dmgMods, spawnWave, spawnDelay):
         self.maxHealth=maxHealth
         self.health=maxHealth
         self.speed=speed
         self.dmgMods=dmgMods
-        self.gridX=0
-        self.gridY=0
+        self.spawnWave=spawnWave
+        self.spawnDelay=spawnDelay
 
-    def Move(self, now, pathWaypoints):
+    def Move(self, now, wave, pathWaypoints):
+        if self.isDead:
+            return
+        if not self.isSpawned:
+            x=0 #calculate whether spawn time has passed and 
+
         elapsed=now-self.lastMove
         #calculate new gridX gridY from current position to next waypoint based on elapsed time; set coordinates to that position
 
     def AcceptAttack(self, attack):
+        if self.isDead:
+            return
         mult=1
         if attack.dmgTypeIndex >=0 and attack.dmgTypeIndex < len(self.dmgMods):
             mult=self.dmgMods[attack.dmgTypeIndex]
--- TdTest/main.py
+++ TdTest/main.py
@@ -1,14 +1,14 @@
 import datetime
-import types
+import gameTypes
 
-global pathWaypoints
 pathWaypoints=[(0,100), (100,100), (100,200), (200,200)]
-nmes=[types.Nme(100, [], pathWaypoints)]
+nmes=[gameTypes.Nme(100, [], pathWaypoints)]
 towers=[]
+wave=0
 
-def MoveNmes(now):
+def MoveEnemies(now):
     for nme in nmes:     
-        nme.Move(now)
+        nme.Move(now, wave)
 
 def ExecuteTowers(now):
     for tower in towers:
@@ -17,8 +17,9 @@ def ExecuteTowers(now):
 isRunning=True
 while (isRunning):
     now = datetime.datetime.now()
-    MoveNmes(now)
-
+    MoveEnemies(now)
+    ExecuteTowers(now)
+    #TODO: determine when wave ends; maybe when all nmes with that spawnWave have spawned?  then when to start a new wave
 
 
 
